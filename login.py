bc97f973bbc454305b28db386906c7612ea90fcb
Sakthivel Mohan <ajithsakthi66@gmail.com>
2026-05-20 16:27:59 +0530

    updated project

---
+++ login.py
@@ -0,0 +1,31 @@
+import getpass
+
+
+class ATMLogin:
+
+    def __init__(self):
+
+        self.user_id = "1234567890"
+        self.pin = "1234"
+
+    def login(self):
+
+        print("=" * 40)
+        print("ATM LOGIN SYSTEM")
+        print("=" * 40)
+
+        entered_user_id = input("Enter User ID: ")
+        entered_pin = getpass.getpass("Enter PIN: ")
+
+        if entered_user_id == self.user_id and entered_pin == self.pin:
+
+            print("\nLogin Successful")
+            print("Welcome to ATM")
+
+        else:
+
+            print("\nInvalid User ID or PIN")
+
+
+atm = ATMLogin()
+atm.login()
